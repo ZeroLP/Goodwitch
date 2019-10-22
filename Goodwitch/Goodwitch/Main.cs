45150cce7087515baf1fa6b03e110a5f1c8b93dc
Zero <stubles227@gmail.com>
2019-10-23 01:55:16 +1100

    Initialise Base Structure

---
+++ Goodwitch/Goodwitch/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goodwitch
+{
+    internal class Main
+    {
+        internal static void Test()
+        {
+            //Do Something...
+        }
+    }
+}
